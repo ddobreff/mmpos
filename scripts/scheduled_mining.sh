d99eeba042d7e1a076b7e9a483d48c8cb355aa0c
Dobromir Dobrev <dobreff@gmail.com>
2024-11-21 17:45:07 +0200

    Update scheduled mining script to fetch MAC addresses from API

--- scripts/scheduled_mining.sh
+++ scripts/scheduled_mining.sh
@@ -8,28 +8,21 @@
 # rig1
 # rig2
 # rig3
-# For mac addresses use your rig mac addresses, obtain them from your dhcp server or using arp.
-# ex:
-# 00:01:02:03:04:05
-# 00:02:03:04:05:06
+# Since agent version 4.0.11 we now post mac_address field to API.
+# Next step is to use tags instead of rig names for full automation.
 CFG="/etc"
 i=0
-while read macs
-do
-    mac[$i]="$macs"
-    i=$((i+1))
-done < ${CFG}/macs.txt
-k=0
 while read rigids
 do
-    rig[$k]="$rigids"
-    k=$((k+1))
+    rigs[$i]="$rigids"
+    i=$((i+1))
 done < ${CFG}/rigs.txt
 
 TCHATID="Your-telegram-chatid-here"
 TBOTID="Your-telegram-botid-here-with-api-key"
 API_TOKEN="MMPOS-API-KEY"
 FARMID="YOUR-MMPOS-FARM-ID"
+LIMIT="100" # Change this if you have more than 100 rigs or profiles.
 # For local setup where you use wakeonlan use the following crontab addition:
 # crontab addition to start at 9am and poweroff at 6pm your local time(make sure your linux system has correct timezone)
 # 0 9 * * * /path/to/script.sh poweron >/dev/null 2>&1
@@ -38,7 +31,7 @@ FARMID="YOUR-MMPOS-FARM-ID"
 # 0 18 * * * /path/to/script.sh schedule_wakeup >/dev/null 2>&1
 # end crontab addition
 # Make sure to edit wakeup time below, its statically set to 9am your local time.
-SCHD=$(date -d "tomorrow 09:00:00" +'%Y-%m-%d %T %Z')
+SCHD=$(date -d "tomorrow 09:30:00" +'%Y-%m-%d %T %Z')
 SCHD_TS=$(date -d "$SCHD" -u +"%s")
 NOW=$(date +'%Y-%m-%d %T %Z')
 
@@ -50,14 +43,21 @@ function send_notification() {
 }
 
 rigUUID=()
-for (( i = 0; i <${#rig[@]} ; i++ )); do
-    rigUUID+=( "$(curl -s -X GET  -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" https://api.mmpos.eu/api/v1/${FARMID}/rigs?limit=100 |jq -r '.[] | select(.name == "'"${rig[$i]}"'") | .id')" )
+macAddrs=()
+for (( i=0; i<${#rigs[@]}; i++ )); do
+    rig="${rigs[i]}"
+    resp=$(curl -s -X GET -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" \
+            "https://api.mmpos.eu/api/v1/${FARMID}/rigs?limit=$LIMIT" | \
+        jq -r --arg name "$rig" '.[] | select(.name == $name) | {id: .id, mac_address: .mac_address}')
+
+    rigUUID[i]="$(echo "$resp" | jq -r '.id')"
+    macAddrs[i]="$(echo "$resp" | jq -r '.mac_address')"
 done
 
 case "$1" in
     poweroff)
-        for (( i = 0; i < ${#rig[@]} ; i++ )); do
-            RIG_STATUS=$(curl -s -X GET -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" "https://api.mmpos.eu/api/v1/${FARMID}/rigs/${rigUUID[$i]}?limit=100" | jq -r .status)
+        for (( i = 0; i < ${#rigs[@]} ; i++ )); do
+            RIG_STATUS=$(curl -s -X GET -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" "https://api.mmpos.eu/api/v1/${FARMID}/rigs/${rigUUID[$i]}?limit=$LIMIT" | jq -r .status)
             if [[ "$RIG_STATUS" != "rig_down" ]]; then
                 curl -s -X POST -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" -d '{"control": "poweroff"}' https://api.mmpos.eu/api/v1/${FARMID}/rigs/${rigUUID[$i]}/control
             else
@@ -67,14 +67,14 @@ case "$1" in
         send_notification "Rigs have been shut at: [ $NOW ]"
         ;;
     schedule_wakeup)
-        for (( i = 0; i < ${#rig[@]} ; i++ )); do
+        for (( i = 0; i < ${#rigs[@]} ; i++ )); do
             curl -s -s -X POST -H "X-API-Key: ${API_TOKEN}" -H "Content-Type: application/json" -d '{"control": "poweroff_wake '"${SCHD_TS}"'"}' https://api.mmpos.eu/api/v1/${FARMID}/rigs/${rigUUID[$i]}/control
         done
         send_notification "Rigs have been shut at: [ $NOW ], but will be powered back up at [ $SCHD ]"
         ;;
     poweron)
-        for (( i = 0; i < ${#mac[@]} ; i++ )); do
-            wakeonlan ${mac[$i]}
+        for (( i = 0; i < ${#macAddrs[@]} ; i++ )); do
+            wakeonlan ${macAddrs[$i]}
         done
         send_notification "Rigs have been manually powered up at: [ $NOW ]"
         ;;
